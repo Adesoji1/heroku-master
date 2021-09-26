09dbbddc929a9ba449d46029a1da91f889b13a6b
Adesoji Alu <adesoji.alu@gmai.com>
2021-09-26 16:31:49 +0100

    This is a submission for curacel test needed for the generated link on heroku and streamlit

--- cur.py
+++ cur.py
@@ -1,22 +1,15 @@
 import tensorflow as tf
 import h5py as h5py
 import streamlit as st
-#from tensorflow.keras.models import Model 
 
-#import os
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1" #To disable run on GPU
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1" #To disable run on GPU
 
 #for relative path
 import os
 from pathlib import Path
 current_directory = Path(__file__).parent #Get current directory
-file = open(os.path.join(current_directory, 'vehicle_detection.h5')
-model = tf.keras.models.load_model.load(file) 
-
-#model = tf.keras.models.load_model('/home/adesoji/hh/curacel/vehicle_detection.h5')
-
-
-
+file = open(os.path.join(current_directory, 'vehicle_detection.h5'))
 st.write("""
          # Bad or Good cars Prediction
          """
@@ -39,8 +32,11 @@ def import_and_predict(image_data, model):
         img_reshape = img_resize[np.newaxis,...]
     
         prediction = model.predict(img_reshape)
-        
         return prediction
+
+model = tf.keras.models.load_model('vehicle_detection.h5')
+        
+      
 if file is None:
     st.text("Please upload an image file")
 else:
